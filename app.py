f2ddf4acd293f4f72bd1d689263a009a287ef4bd
TiceWise <46386345+TiceWise@users.noreply.github.com>
2021-05-18 15:43:14 +0200

    setting up basic files

--- app.py
+++ app.py
@@ -1,10 +1,25 @@
-from flask import Flask
+from flask import Flask, render_template, redirect, request, session, make_response,session,redirect
+import spotipy.util as util
+import time
+import json
+
 app = Flask(__name__)
+app.config.from_pyfile('config.py')
+app.secret_key = app.config['SECRET_SESSION_KEY']
+# help from:
+# https://stackoverflow.com/questions/57580411/storing-spotify-token-in-flask-session-using-spotipy
+# https://medium.com/analytics-vidhya/discoverdaily-a-flask-web-application-built-with-the-spotify-api-and-deployed-on-google-cloud-6c046e6e731b
+
 
 @app.route('/')
 def hello_world():
-    app.config['TESTING'] = True
-    return 'Hello, World!'
+    session.clear()
+    session['token'] = util.prompt_for_user_token("",
+                                                 scope=app.config['SCOPE'],
+                                                 client_id=app.config['CLIENT_ID'],
+                                                 client_secret=app.config['CLIENT_SECRET'],
+                                                 redirect_uri="http://127.0.0.1:5000/index")
+    return redirect("/index")
 
 
 @app.route('/authorize')
