1312508f4c152be4d5d6597428037dfd41902722
TiceWise <46386345+TiceWise@users.noreply.github.com>
2021-05-23 03:32:37 +0200

    Login and select source implemented

--- app.py
+++ app.py
@@ -1,87 +1,101 @@
-from flask import Flask, render_template, redirect, request, session, make_response,session,redirect
+from flask import Flask, render_template, redirect, request, session, flash
 import spotipy
-import spotipy.util as util
-import time
-import json
+import os
+import uuid
 
 app = Flask(__name__)
 app.config.from_pyfile('config.py')
 app.secret_key = app.config['SECRET_SESSION_KEY']
+
 # help from:
 # https://stackoverflow.com/questions/57580411/storing-spotify-token-in-flask-session-using-spotipy
 # https://medium.com/analytics-vidhya/discoverdaily-a-flask-web-application-built-with-the-spotify-api-and-deployed-on-google-cloud-6c046e6e731b
+# https://www.digitalocean.com/community/tutorials/how-to-make-a-web-application-using-flask-in-python-3
+# https://github.com/plamere/spotipy/blob/master/examples/app.py
 
+caches_folder = './.spotify_caches/'
+if not os.path.exists(caches_folder):
+    os.makedirs(caches_folder)
 
-@app.route('/')
-def verify():
-    # Don't reuse a SpotifyOAuth object because they store token info and you could leak user tokens if you reuse a SpotifyOAuth object
-    sp_oauth = spotipy.oauth2.SpotifyOAuth(client_id = app.config['CLIENT_ID'],
+def session_cache_path():
+    return caches_folder + session.get('uuid')
+
+def check_log_in():
+    cache_handler = spotipy.cache_handler.CacheFileHandler(cache_path=session_cache_path())
+    auth_manager = spotipy.oauth2.SpotifyOAuth(client_id = app.config['CLIENT_ID'],
                                            client_secret = app.config['CLIENT_SECRET'],
                                            redirect_uri = app.config['REDIRECT_URI'],
-                                           scope = app.config['SCOPE'])
-    auth_url = sp_oauth.get_authorize_url()
-    print(auth_url)
-    return redirect(auth_url)
-
-
-# authorization-code-flow Step 2.
-# Have your application request refresh and access tokens;
-# Spotify returns access and refresh tokens
-@app.route("/api_callback")
-def api_callback():
-    # Don't reuse a SpotifyOAuth object because they store token info and you could leak user tokens if you reuse a SpotifyOAuth object
-    sp_oauth = spotipy.oauth2.SpotifyOAuth(client_id = app.config['CLIENT_ID'],
-                                           client_secret = app.config['CLIENT_SECRET'],
-                                           redirect_uri = app.config['REDIRECT_URI'],
-                                           scope = app.config['SCOPE'])
-    session.clear()
-    code = request.args.get('code')
-    token_info = sp_oauth.get_access_token(code)
-
-    # Saving the access token along with all other token related info
-    session["token_info"] = token_info
-
-    return redirect("index")
-
-# authorization-code-flow Step 3.
-# Use the access token to access the Spotify Web API;
-# Spotify returns requested data
-@app.route("/go", methods=['POST'])
-def go():
-    session['token_info'], authorized = get_token(session)
-    session.modified = True
-    if not authorized:
+                                           scope = app.config['SCOPE'],
+                                           cache_handler=cache_handler,
+                                           show_dialog=True)
+    if not auth_manager.validate_token(cache_handler.get_cached_token()):
         return redirect('/')
-    data = request.form
-    sp = spotipy.Spotify(auth=session.get('token_info').get('access_token'))
-    response = sp.current_user_top_tracks(limit=data['num_tracks'], time_range=data['time_range'])
 
-    # print(json.dumps(response))
+    return spotipy.Spotify(auth_manager=auth_manager)
 
-    return render_template("results.html", data=data)
+# authorization-code-flow Step 1. Have your application request authorization;
+# the user logs in and authorizes access
+@app.route('/')
+def index():
+    if not session.get('uuid'):
+        # Step 1. Visitor is unknown, give random ID
+        session['uuid'] = str(uuid.uuid4())
 
-# Checks to see if token is valid and gets a new token if not
-def get_token(session):
-    token_valid = False
-    token_info = session.get("token_info", {})
+    cache_handler = spotipy.cache_handler.CacheFileHandler(cache_path=session_cache_path())
 
-    # Checking if the session already has a token stored
-    if not (session.get('token_info', False)):
-        token_valid = False
-        return token_info, token_valid
+    # Don't reuse a SpotifyOAuth object because they store token info and you could leak user tokens if you reuse a SpotifyOAuth object
+    auth_manager = spotipy.oauth2.SpotifyOAuth(client_id = app.config['CLIENT_ID'],
+                                           client_secret = app.config['CLIENT_SECRET'],
+                                           redirect_uri = app.config['REDIRECT_URI'],
+                                           scope = app.config['SCOPE'],
+                                           cache_handler=cache_handler,
+                                           show_dialog=True)
 
-    # Checking if token has expired
-    now = int(time.time())
-    is_token_expired = session.get('token_info').get('expires_at') - now < 60
+    if request.args.get("code"):
+        # Step 3. Being redirected from Spotify auth page
+        auth_manager.get_access_token(request.args.get("code"))
+        return redirect('/')
 
-    # Refreshing token if it has expired
-    if (is_token_expired):
-        # Don't reuse a SpotifyOAuth object because they store token info and you could leak user tokens if you reuse a SpotifyOAuth object
-        sp_oauth = spotipy.oauth2.SpotifyOAuth(client_id = CLI_ID, client_secret = CLI_SEC, redirect_uri = REDIRECT_URI, scope = SCOPE)
-        token_info = sp_oauth.refresh_access_token(session.get('token_info').get('refresh_token'))
+    if not auth_manager.validate_token(cache_handler.get_cached_token()):
+        # Step 2. Display sign in link when no token
+        auth_url = auth_manager.get_authorize_url()
 
-    token_valid = True
-    return token_info, token_valid
+        return render_template('login.html',auth_url=auth_url)
+
+    # Step 4. Signed in, display data
+    session['login_success'] = True
+    spotify = spotipy.Spotify(auth_manager=auth_manager)
+    return redirect('/select_source')
+
+
+@app.route("/select_source", methods=["GET", "POST"])
+def select_source():
+    # check if we are correctly logged in
+    spotify = check_log_in()
+
+    if request.method == "POST":
+        print(request.form['playlist_id'])
+        return request.form['playlist_id']
+
+    else:
+        user = spotify.me()
+        username = user['display_name']
+
+        pl = spotify.current_user_playlists()
+        # loop till we get all playlists...
+        playlists = pl['items']
+        i = 0
+        while pl['next']:
+            pl = spotify.next(pl)
+            playlists.extend(pl['items'])
+
+        # for playlist in playlists:
+        #     print(playlist['name'])
+
+        return render_template("select_source.html", username=username, playlists=playlists)
+
+
+
 
 if __name__ == "__main__":
     app.run(debug=True)
--- config.py
+++ config.py
@@ -1,16 +1,18 @@
 """Flask configuration."""
-from os import environ, path
+import os
 
 TESTING = True
 DEBUG = True
-SHOW_DIALOG =True
+SHOW_DIALOG = True
+SESSION_TYPE = 'filesystem'
+SESSION_FILE_DIR = './.flask_session/'
 
 FLASK_ENV = 'development'
-CLIENT_ID = environ.get('SPODI_CLIENT_ID')
-CLIENT_SECRET = environ.get('SPODI_CLIENT_SECRET')
-SECRET_SESSION_KEY = environ.get('SECRET_SESSION_KEY')
-SCOPE = 'playlist-modify-private,playlist-modify-public,user-library-modify'
+CLIENT_ID = os.environ.get('SPOTIPY_CLIENT_ID')
+CLIENT_SECRET = os.environ.get('SPOTIPY_CLIENT_SECRET')
+SECRET_SESSION_KEY = os.environ.get('SECRET_SESSION_KEY')
+SCOPE = 'playlist-read-private,playlist-modify-private,playlist-read-collaborative,playlist-modify-public,user-library-read,user-library-modify'
 API_BASE = 'https://accounts.spotify.com'
 
 # Make sure you add this to Redirect URIs in the setting of the application dashboard
-REDIRECT_URI = "http://127.0.0.1:5000/api_callback"
+REDIRECT_URI = "http://127.0.0.1:5000/"
