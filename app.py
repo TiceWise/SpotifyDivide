4bb0ec1e2f567d68b4d9f6fb2a0d661dd472c893
TiceWise <46386345+TiceWise@users.noreply.github.com>
2021-05-24 13:45:27 +0200

    setting up vs code and ignores (second time)

--- app.py
+++ app.py
@@ -4,8 +4,8 @@ import os
 import uuid
 
 app = Flask(__name__)
-app.config.from_pyfile('config.py')
-app.secret_key = app.config['SECRET_SESSION_KEY']
+app.config.from_pyfile("config.py")
+app.secret_key = app.config["SECRET_SESSION_KEY"]
 
 # help from:
 # https://stackoverflow.com/questions/57580411/storing-spotify-token-in-flask-session-using-spotipy
@@ -13,65 +13,80 @@ app.secret_key = app.config['SECRET_SESSION_KEY']
 # https://www.digitalocean.com/community/tutorials/how-to-make-a-web-application-using-flask-in-python-3
 # https://github.com/plamere/spotipy/blob/master/examples/app.py
 
-caches_folder = './.spotify_caches/'
+caches_folder = "./.spotify_caches/"
 if not os.path.exists(caches_folder):
     os.makedirs(caches_folder)
 
+
 def session_cache_path():
-    return caches_folder + session.get('uuid')
+    return caches_folder + session.get("uuid")
+
 
 def check_log_in():
-    cache_handler = spotipy.cache_handler.CacheFileHandler(cache_path=session_cache_path())
-    auth_manager = spotipy.oauth2.SpotifyOAuth(client_id = app.config['CLIENT_ID'],
-                                           client_secret = app.config['CLIENT_SECRET'],
-                                           redirect_uri = app.config['REDIRECT_URI'],
-                                           scope = app.config['SCOPE'],
-                                           cache_handler=cache_handler,
-                                           show_dialog=True)
+    cache_handler = spotipy.cache_handler.CacheFileHandler(
+        cache_path=session_cache_path()
+    )
+    auth_manager = spotipy.oauth2.SpotifyOAuth(
+        client_id=app.config["CLIENT_ID"],
+        client_secret=app.config["CLIENT_SECRET"],
+        redirect_uri=app.config["REDIRECT_URI"],
+        scope=app.config["SCOPE"],
+        cache_handler=cache_handler,
+        show_dialog=True,
+    )
+
     if not auth_manager.validate_token(cache_handler.get_cached_token()):
-        return redirect('/')
+        return redirect("/")
 
     return spotipy.Spotify(auth_manager=auth_manager)
 
+
 # authorization-code-flow Step 1. Have your application request authorization;
 # the user logs in and authorizes access
-@app.route('/')
+
+
+@app.route("/")
 def index():
-    if not session.get('uuid'):
+    if not session.get("uuid"):
         # Step 1. Visitor is unknown, give random ID
-        session['uuid'] = str(uuid.uuid4())
+        session["uuid"] = str(uuid.uuid4())
 
-    cache_handler = spotipy.cache_handler.CacheFileHandler(cache_path=session_cache_path())
+    cache_handler = spotipy.cache_handler.CacheFileHandler(
+        cache_path=session_cache_path()
+    )
 
-    # Don't reuse a SpotifyOAuth object because they store token info and you could leak user tokens if you reuse a SpotifyOAuth object
-    auth_manager = spotipy.oauth2.SpotifyOAuth(client_id = app.config['CLIENT_ID'],
-                                           client_secret = app.config['CLIENT_SECRET'],
-                                           redirect_uri = app.config['REDIRECT_URI'],
-                                           scope = app.config['SCOPE'],
-                                           cache_handler=cache_handler,
-                                           show_dialog=True)
+    # Don't reuse a SpotifyOAuth object because they store token info and you
+    # could leak user tokens if you reuse a SpotifyOAuth object
+    auth_manager = spotipy.oauth2.SpotifyOAuth(
+        client_id=app.config["CLIENT_ID"],
+        client_secret=app.config["CLIENT_SECRET"],
+        redirect_uri=app.config["REDIRECT_URI"],
+        scope=app.config["SCOPE"],
+        cache_handler=cache_handler,
+        show_dialog=True,
+    )
 
     if request.args.get("code"):
         # Step 3. Being redirected from Spotify auth page
         auth_manager.get_access_token(request.args.get("code"))
-        return redirect('/')
+        return redirect("/")
 
     if not auth_manager.validate_token(cache_handler.get_cached_token()):
         # Step 2. Display sign in link when no token
         auth_url = auth_manager.get_authorize_url()
 
-        return render_template('login.html',auth_url=auth_url)
+        return render_template("login.html", auth_url=auth_url)
 
     # Step 4. Signed in, display data
-    session['login_success'] = True
+    session["login_success"] = True
     spotify = spotipy.Spotify(auth_manager=auth_manager)
 
     user = spotify.me()
-    username = user['display_name']
+    username = user["display_name"]
 
     flash(f"Logged in to Spotify as {username}")
 
-    return redirect('/select_source')
+    return redirect("/select_source")
 
 
 @app.route("/select_source", methods=["GET", "POST"])
@@ -80,25 +95,21 @@ def select_source():
     spotify = check_log_in()
 
     if request.method == "POST":
-        print(request.form['playlist_id'])
-        return request.form['playlist_id']
-
+        print(request.form["playlist_id"])
+        return request.form["playlist_id"]
     else:
-
         pl = spotify.current_user_playlists()
         # loop till we get all playlists...
-        playlists = pl['items']
-        i = 0
-        while pl['next']:
+        playlists = pl["items"]
+
+        while pl["next"]:
             pl = spotify.next(pl)
-            playlists.extend(pl['items'])
+            playlists.extend(pl["items"])
 
         # for playlist in playlists:
         #     print(playlist['name'])
 
         return render_template("select_source.html", playlists=playlists)
-
-
 
 
 if __name__ == "__main__":
