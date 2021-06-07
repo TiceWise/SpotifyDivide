378c1e9fe69195458c324558b1586679cfdb2170
TiceWise <46386345+TiceWise@users.noreply.github.com>
2021-06-07 15:59:11 +0200

    wrong directory names

--- app.py
+++ app.py
@@ -60,7 +60,7 @@ Session(app)
 #         return None
 
 
-caches_folder = "./.spotify_caches"
+caches_folder = "./.spotify_caches/"
 if not os.path.exists(caches_folder):
     os.makedirs(caches_folder)
 
